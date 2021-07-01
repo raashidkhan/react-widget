5afd3d7b9c0d74519082235f68019bc8f60d3b09
Raashid Khan <raashid@cobold.in>
2021-07-01 20:29:36 +0530

    inital widget

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Find all widget divs
+const WidgetDivs = document.querySelectorAll(".reddit_widget");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Inject our React App into each
+WidgetDivs.forEach((Div) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App domElement={Div} />
+    </React.StrictMode>,
+    Div
+  );
+});
